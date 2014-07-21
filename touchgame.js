0ac6718b3f2c324f29037fa179ed706ecfec935a
Group 3 Summer School <group3summerschool@gmail.com>
2014-07-21 15:04:13 +0200

    Added mobile version

---
+++ touchgame.js
@@ -0,0 +1,298 @@
+var canvas = document.getElementById("the-game");
+var context = canvas.getContext("2d");
+var game, snake, food;
+
+game = {
+  
+  score: 0,
+  fps: 8,
+  over: false,
+  message: null,
+  
+  start: function() {
+    game.over = false;
+    game.message = null;
+    game.score = 0;
+    game.fps = 8;
+    snake.init();
+    food.set();
+  },
+  
+  stop: function() {
+    game.over = true;
+    game.message = 'Your score: '+game.score;
+  },
+  
+  drawBox: function(x, y, size, color) {
+    context.fillStyle = color;
+    context.beginPath();
+    context.moveTo(x - (size / 2), y - (size / 2));
+    context.lineTo(x + (size / 2), y - (size / 2));
+    context.lineTo(x + (size / 2), y + (size / 2));
+    context.lineTo(x - (size / 2), y + (size / 2));
+    context.closePath();
+    context.fill();
+  },
+  
+  drawScore: function() {
+    context.fillStyle = '#999';
+    context.font = (canvas.height) + 'px Impact, sans-serif';
+    context.textAlign = 'center';
+    context.fillText(game.score, canvas.width / 2, canvas.height * 0.9);
+  },
+  
+  drawMessage: function() {
+    if (game.message !== null) {
+      context.fillStyle = '#00F';
+      context.strokeStyle = '#FFF';
+      context.font = (canvas.height / 10) + 'px Impact';
+      context.textAlign = 'center';
+      context.fillText(game.message, canvas.width / 2, canvas.height / 2);
+      context.strokeText(game.message, canvas.width / 2, canvas.height / 2);
+    }
+  },
+  
+  resetCanvas: function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  
+};
+
+snake = {
+  
+  size: canvas.width / 40,
+  x: null,
+  y: null,
+  color: '#0F0',
+  direction: 'left',
+  sections: [],
+  
+  init: function() {
+    snake.sections = [];
+    snake.direction = 'left';
+    snake.x = canvas.width / 2 + snake.size / 2;
+    snake.y = canvas.height / 2 + snake.size / 2;
+    for (var i = snake.x + (5 * snake.size); i >= snake.x; i -= snake.size) {
+      snake.sections.push(i + ',' + snake.y); 
+    }
+  },
+  
+  move: function() {
+    switch (snake.direction) {
+      case 'up':
+        snake.y -= snake.size;
+        break;
+      case 'down':
+        snake.y += snake.size;
+        break;
+      case 'left':
+        snake.x -= snake.size;
+        break;
+      case 'right':
+        snake.x += snake.size;
+        break;
+    }
+    snake.checkCollision();
+    snake.checkGrowth();
+    snake.sections.push(snake.x + ',' + snake.y);
+  },
+  
+  draw: function() {
+    for (var i = 0; i < snake.sections.length; i++) {
+      snake.drawSection(snake.sections[i].split(','));
+    }    
+  },
+  
+  drawSection: function(section) {
+    game.drawBox(parseInt(section[0]), parseInt(section[1]), snake.size, snake.color);
+  },
+  
+  checkCollision: function() {
+    if (snake.isCollision(snake.x, snake.y) === true) {
+      game.stop();
+    }
+  },
+  
+  isCollision: function(x, y) {
+    if (x < snake.size / 2 ||
+        x > canvas.width ||
+        y < snake.size / 2 ||
+        y > canvas.height ||
+        snake.sections.indexOf(x + ',' + y) >= 0) {
+      return true;
+    }
+  },
+  
+  checkGrowth: function() {
+    if (snake.x == food.x && snake.y == food.y) {
+      game.score++;
+      if (game.score % 5 == 0 && game.fps < 60) {
+        game.fps++;
+      }
+      food.set();
+    } else {
+      snake.sections.shift();
+    }
+  }
+  
+};
+
+// FOOD
+food = {
+  
+  size: null,
+  x: null,
+  y: null,
+  color: '#0FF',
+  
+  set: function() {
+    food.size = snake.size;
+    food.x = (Math.ceil(Math.random() * 10) * snake.size * 4) - snake.size / 2;
+    food.y = (Math.ceil(Math.random() * 10) * snake.size * 3) - snake.size / 2;
+  },
+  
+  draw: function() {
+    game.drawBox(food.x, food.y, food.size, food.color);
+  }
+  
+};
+
+var inverseDirection = {
+  'up': 'down',
+  'left': 'right',
+  'right': 'left',
+  'down': 'up'
+};
+
+addEventListener('load', function(){
+			var el = document.getElementById('the-game')
+			var status = document.getElementById('status')
+			var hidetimer = null
+			swipedetect(el, function(swipedir){
+				if (swipedir != 'none'){
+					clearTimeout(hidetimer)
+					status.innerHTML = 'Swipe: <span style="color:red">' + swipedir + ' swipe!</span>'
+					var lastKey = swipedir
+					if (['up', 'down', 'left', 'right'].indexOf(lastKey) >= 0
+						&& lastKey != inverseDirection[snake.direction]) {
+					  snake.direction = lastKey;
+					  }
+
+					hidetimer = setTimeout(function(){
+						// inner.style.background = ''
+					}, 1000)
+				}
+
+			})
+
+		}, false)
+
+var requestAnimationFrame = window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame;
+
+function loop() {
+  if (game.over == false) {
+    game.resetCanvas();
+    game.drawScore();
+    snake.move();
+    food.draw();
+    snake.draw();
+    game.drawMessage();
+  }
+  setTimeout(function() {
+    requestAnimationFrame(loop);
+  }, 1000 / game.fps);
+}
+
+function swipedetect(el, callback){
+	var touchsurface = document.getElementById('the-game'),
+			swipedir,
+			startX,
+			startY,
+			distX,
+			distY,
+			threshold = 150,
+			restraint = 100,
+			allowedTime = 300,
+			elapsedTime,
+			startTime,
+			ismousedown = false,
+			detecttouch = !!('ontouchstart' in window) || !!('ontouchstart' in document.documentElement) || !!window.ontouchstart || !!window.Touch || !!window.onmsgesturechange || (window.DocumentTouch && window.document instanceof window.DocumentTouch),
+			handleswipe = callback || function(swipedir){}
+
+	touchsurface.addEventListener('touchstart', function(e){
+		var touchobj = e.changedTouches[0]
+		swipedir = 'none'
+		dist = 0
+		startX = touchobj.pageX
+		startY = touchobj.pageY
+		startTime = new Date().getTime() // record time when finger first makes contact with surface
+		e.preventDefault()
+
+	}, false)
+
+	touchsurface.addEventListener('touchmove', function(e){
+		e.preventDefault() // prevent scrolling when inside DIV
+	}, false)
+
+	touchsurface.addEventListener('touchend', function(e){
+		var touchobj = e.changedTouches[0]
+		distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+		distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+		elapsedTime = new Date().getTime() - startTime // get time elapsed
+		if (elapsedTime <= allowedTime){ // first condition for awipe met
+			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+				swipedir = (distX < 0)? 'left' : 'right'
+			}
+			else if (Math.abs(distY) >= threshold  && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+				swipedir = (distY < 0)? 'up' : 'down'
+			}
+		}
+		// check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+		handleswipe(swipedir)
+		e.preventDefault()
+	}, false)
+
+	if (!detecttouch){
+		document.body.addEventListener('mousedown', function(e){
+			if ( isContained(touchsurface, e) ){
+				var touchobj = e
+				swipedir = 'none'
+				dist = 0
+				startX = touchobj.pageX
+				startY = touchobj.pageY
+				startTime = new Date().getTime() // record time when finger first makes contact with surface
+				ismousedown = true
+				e.preventDefault()
+			}
+		}, false)
+
+		document.body.addEventListener('mousemove', function(e){
+			e.preventDefault() // prevent scrolling when inside DIV
+		}, false)
+
+		document.body.addEventListener('mouseup', function(e){
+			if (ismousedown){
+				var touchobj = e
+				distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+				distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+				elapsedTime = new Date().getTime() - startTime // get time elapsed
+				if (elapsedTime <= allowedTime){ // first condition for awipe met
+					if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+						swipedir = (distX < 0)? 'left' : 'right'
+					}
+					else if (Math.abs(distY) >= threshold  && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+						swipedir = (distY < 0)? 'up' : 'down'
+					}
+				}
+				// check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+				handleswipe(swipedir)
+				ismousedown = false
+				e.preventDefault()
+			}
+		}, false)
+	}
+}
+
+requestAnimationFrame(loop);
